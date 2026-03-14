cbec33f78ba6aa59a640021a648e42fde3aa1be6
markhabaerzhan <markhaba.erzhankyzy@narxoz.kz>
2026-03-14 17:01:05 +0500

    Add RaidResult

--- src/com/narxoz/rpg/battle/RaidResult.java
+++ src/com/narxoz/rpg/battle/RaidResult.java
@@ -12,23 +12,18 @@ public class RaidResult {
     public String getWinner() {
         return winner;
     }
-
     public void setWinner(String winner) {
         this.winner = winner;
     }
-
     public int getRounds() {
         return rounds;
     }
-
     public void setRounds(int rounds) {
         this.rounds = rounds;
     }
-
     public void addLine(String line) {
         log.add(line);
     }
-
     public List<String> getLog() {
         return Collections.unmodifiableList(log);
     }
